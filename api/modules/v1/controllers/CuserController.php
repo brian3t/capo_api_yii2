b4455762a832e73f76f201d62e27c02ffd7afe7c
brian3t <ngxtri@gmail.com>
2016-06-22 14:52:16 -0700

    Return cuser id after saving

--- api/modules/v1/controllers/CuserController.php
+++ api/modules/v1/controllers/CuserController.php
@@ -10,11 +10,11 @@ use yii\web\HttpException;
 
 class CuserController extends ActiveController
 {
-    public $modelClass='app\models\Cuser';
+    public $modelClass = 'app\models\Cuser';
 
     public function actions()
     {
-        $actions=parent::actions();
+        $actions = parent::actions();
         unset($actions['create']);
         return $actions;
     }
@@ -23,14 +23,14 @@ class CuserController extends ActiveController
     {
         return ArrayHelper::merge([
             [
-                'class'=>Cors::className(),
-                'cors'=>[
-                    'Origin'=>['*'],
-                    'Access-Control-Request-Methods'=>['GET','POST','OPTIONS','DELETE','PUT'],
-                    'Access-Control-Request-Headers'=>['Content-Type']
+                'class' => Cors::className(),
+                'cors' => [
+                    'Origin' => ['*'],
+                    'Access-Control-Request-Methods' => ['GET', 'POST', 'OPTIONS', 'DELETE', 'PUT'],
+                    'Access-Control-Request-Headers' => ['Content-Type']
                 ],
             ],
-        ],parent::behaviors());
+        ], parent::behaviors());
     }
 
     public function actionCreate()
@@ -38,26 +38,34 @@ class CuserController extends ActiveController
         \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
         header("Access-Control-Allow-Origin: *");
 
-        $entityBody=file_get_contents('php://input');
-        try
-        {
-            $entityBody=json_decode($entityBody);
+        $entityBody = file_get_contents('php://input');
+        try {
+            $entityBody = json_decode($entityBody);
             //if cuser exists, return its id
-//            $cuser = Cuser::findOne(['commuter'=>$entityBody->commuter]);
-//            $cuser  = Cuser::find()->where(['id'=>'571317eeb6f15571317eeb6f1a']);
-            $cuser  = Cuser::find()->where(array('commuter'=>intval($entityBody->commuter)));
+            $cuser = Cuser::find()->where(array('commuter' => intval($entityBody->commuter)))->one();
+            if (is_object($cuser) && isset($cuser->id)) {
+                echo '{"status":"successful", "id":"' . $cuser->id . '"}';
+                \Yii::$app->response->setStatusCode(200);
+                return;
+            }
             //else, try creating one
-        } catch (Exception $e)
-        {
+            $new_cuser = new Cuser();
+            $new_cuser->load(['Cuser' => (array)$entityBody]);
+            try {
+                $new_cuser->save();
+                echo '{"status":"successful", "id":"' . $new_cuser->id . '"}';
+                \Yii::$app->response->setStatusCode(201);
+                return;
+            } catch (Exception $e) {
+                \Yii::error("Cant create new cuser " . json_encode($entityBody) . $e->getMessage());
+                \Yii::$app->response->setStatusCode(400);
+                return;
+            }
+        } catch (Exception $e) {
             \Yii::error("Bad input " . $e->getMessage());
-//            echo '{"status":"successful"}';
-            \Yii::$app->response->setStatusCode(404);
-//            throw new HttpException(404,'Bad incoming data!');
+            \Yii::$app->response->setStatusCode(400);
             return;
         }
-
-        echo '{"status":"successful"}';
-        return;
     }
 
 }

6f38d0840766f392b9bfe5324b1b66778ed4bef6
brian3t <ngxtri@gmail.com>
2016-09-08 14:23:10 -0700

    Cron to delete idle requests from Oracle DB.

---
+++ cron/del_idle_request.php
@@ -0,0 +1,53 @@
+<?php
+
+$log = fopen(__DIR__."/logs/del_idle_request.txt", "a");
+$message = '';
+
+/* SELECT * from "request" WHERE "updated_at" < (SYSDATE - INTERVAL '10' MINUTE);*/
+
+error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+
+$conn = oci_connect('carpoolnowdb', 'Duip34jitjit-', '52.9.243.54/ORCL');
+
+// $stid = oci_parse($conn, 'select table_name from user_tables');
+// oci_execute($stid);
+//
+// while (($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+//     echo "<tr>\n";
+//     foreach ($row as $item) {
+//         echo "  <td>".($item !== null ? htmlspecialchars($item, ENT_QUOTES) : "&nbsp;")."</td>\n";
+//     }
+//     echo "</tr>\n";
+// }
+//
+//
+// $stid = oci_parse($conn, 'select * from "cuser"');
+// oci_execute($stid);
+//
+// while (($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+//     echo "<tr>\n";
+//     foreach ($row as $item) {
+//         echo "  <td>".($item !== null ? htmlspecialchars($item, ENT_QUOTES) : "&nbsp;")."</td>\n";
+//     }
+//     echo "</tr>\n";
+// }
+
+$stid = oci_parse($conn, 'SELECT * from "request" WHERE "updated_at" < (SYSDATE - INTERVAL \'10\' MINUTE) ');
+oci_execute($stid);
+
+while (($row = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+    $message .= "Deleting: ";
+    foreach ($row as $item) {
+        $message .= ($item !== null ? htmlspecialchars($item, ENT_QUOTES) : "&nbsp;")."\r\n";
+    }
+    $message .= "\r\n";
+}
+
+$stid = oci_parse($conn, 'DELETE from "request" WHERE "updated_at" < (SYSDATE - INTERVAL \'10\' MINUTE) ');
+oci_execute($stid);
+
+
+$message .= "____________________________________________\r\n";
+fwrite($log, $message);
+

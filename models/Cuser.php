489d7cd754e8e76dced4b3feed16d08d47edc4df
brian3t <ngxtri@gmail.com>
2016-09-18 23:08:23 -0700

    Don't return updated_at for cuser.

--- models/Cuser.php
+++ models/Cuser.php
@@ -60,5 +60,10 @@ class Cuser extends BaseCuser
         }
     }
     
-    
+    public function fields()
+    {
+        $fields = parent::fields();
+        unset($fields['updated_at']);
+        return $fields;
+    }
 }
